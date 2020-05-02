b6f3e5d19b7e5e1d90117e8f3b363646240fc678
kremrik <kyle.robert.e@gmail.com>
2020-05-02 14:27:29 -0500

    added simple timings for fun

--- merge.py
+++ merge.py
@@ -1,11 +1,16 @@
 def merge(raw: dict, master: dict) -> dict:
+    """
+    raw = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
+    master = {"two": int, "three": int, "four": int, "five": int, "six": int}
+    2 µs ± 24.1 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+    """
     if not master:
         return master
 
     output = {}
     raw_keys = raw.keys()
     master_keys = master.keys()
-    
+
 
     for m_key, m_type in master.items():
         if m_key in raw.keys():
@@ -20,12 +25,12 @@ def merge(raw: dict, master: dict) -> dict:
         else:
             msg = f"Missing key '{m_key}'"
             output[m_key] = None
-        print(msg)
+        # print(msg)
 
     for r_key, r_val in raw.items():
         if r_key not in master.keys():
             msg = f"Key '{r_key}' was not in master schema"
             output[r_key] = r_val
-        print(msg)
+        # print(msg)
 
     return output
