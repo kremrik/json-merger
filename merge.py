9f80e2046708e9fff91dfc57822dc15ac2f96b41
kremrik <kyle.robert.e@gmail.com>
2020-05-03 20:28:55 -0500

    final version for now

--- merge.py
+++ merge.py
@@ -2,16 +2,6 @@ from typing import Any, Callable
 
 
 def merge(raw: dict, master: dict) -> dict:
-    """
-    raw = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
-    master = {"two": int, "three": int, "four": int, "five": int, "six": int}
-
-    # 2 µs ± 24.1 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
-    # 1.42 µs ± 4.29 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
-    1.16 µs ± 39.1 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
-
-    > 40% reduction in run time
-    """
     output = {}
 
     # adds keys from `master` if they do not exist in `raw`
