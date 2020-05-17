6c53210e936f6aaea9b876f91f681606163ff078
kremrik <kyle.robert.e@gmail.com>
2020-05-17 13:27:41 -0500

    refactored to more functional approach

--- merge.py
+++ merge.py
@@ -1,29 +1,81 @@
-from typing import Callable
+from functools import partial
+from typing import Any, Callable, Optional, Sequence, Tuple
 
 
-def merge(dict1: dict, dict2: dict, array_strategy: Callable = lambda x, y: x) -> dict:
-    output = {}
+def merge(
+    dict1: dict, 
+    dict2: dict, 
+    dict_strategy: Callable = lambda x, y: x,
+    list_strategy: Callable = lambda x, y: x
+    ) -> dict:
+    """
+    `merge` is a function to recursively, uhh, merge two dictionaries
 
-    # adds keys from `dict2` if they do not exist in `dict1`
-    intersection = {**dict2, **dict1}
+    By default, `merge` operates like the expression ``{**dict1, **dict2}``, 
+    but when supplied functions for `dict_strategy` and `list_strategy` it 
+    is capable of merging any Python dict in any way imaginable. You can 
+    choose from some predefined functions from the `strategies` module to 
+    get started quickly, or roll your own for even more fun. 
 
-    for k_intersect, v_intersect in intersection.items():
-        if k_intersect not in dict1:
-            v_dict2 = dict2[k_intersect]
-            output[k_intersect] = v_dict2
+    TODO: add `strategies` module
+    """
 
-        elif k_intersect not in dict2:
-            output[k_intersect] = v_intersect
+    keys = get_keys(dict1, dict2)
+    return {
+        key: merge_map(
+            *get_values(dict1, dict2, key), 
+            dict_strategy, 
+            list_strategy)
+        for key in keys
+    }
 
-        elif isinstance(v_intersect, dict):
-            v_dict2 = dict2[k_intersect]
-            output[k_intersect] = merge(v_intersect, v_dict2)
 
-        elif isinstance(v_intersect, list):
-            v_dict2 = dict2[k_intersect]
-            output[k_intersect] = array_strategy(v_intersect, v_dict2)
+def get_keys(left: dict, right: dict) -> set:
+    return left.keys() | right.keys()
 
-        else:
-            output[k_intersect] = v_intersect
 
-    return output
+def get_values(left: dict, right: dict, key: str) -> Tuple[Any, Any]:
+    return get_value_from_dict(left, key), get_value_from_dict(right, key)
+
+
+def get_value_from_dict(d: dict, key: str) -> Optional[Any]:
+    if key in d:
+        return d[key]
+    return None
+
+
+def merge_map(
+    left: Any, 
+    right: Any, 
+    dict_strategy: Callable = lambda x, y: x, 
+    list_strategy: Callable = lambda x, y: x
+    ):
+
+    if not (left and right):
+        return first(
+            list(
+                filter(lambda x: bool(x), [left, right])
+            )
+        )
+
+    if is_dict(left):
+        return dict_strategy(left, right)
+
+    if is_list(left):
+        return list_strategy(left, right)
+    
+    return left
+
+
+def first(obj: Sequence) -> Optional[Any]:
+    if not obj:
+        return None
+    return obj[0]
+
+
+def is_instance_of(value: Any, obj: Any) -> bool:
+    return isinstance(value, obj)
+
+is_dict = partial(is_instance_of, obj=dict)
+
+is_list = partial(is_instance_of, obj=list)
--- test_merge.py
+++ test_merge.py
@@ -1,4 +1,4 @@
-from merge import merge
+from merge import merge, merge_map, get_keys, get_values
 import unittest
 from unittest import skip
 from functools import reduce
@@ -34,6 +34,26 @@ class test_merge(unittest.TestCase):
         output = merge(raw, master)
         self.assertEqual(gold, output)
 
+    def test_nested_master_has_more_default(self):
+        raw = {
+            "foo": {
+                "bar": 1
+            }
+        }
+        master = {
+            "foo": {
+                "bar": None,
+                "baz": None
+            }
+        }
+        gold = {
+            "foo": {
+                "bar": 1
+            }
+        }
+        output = merge(raw, master)
+        self.assertEqual(gold, output)
+
     def test_nested_master_has_more(self):
         raw = {
             "foo": {
@@ -52,7 +72,11 @@ class test_merge(unittest.TestCase):
                 "baz": None
             }
         }
-        output = merge(raw, master)
+
+        def dict_strat(dict1, dict2) -> dict:
+            return merge(dict1, dict2)
+
+        output = merge(raw, master, dict_strategy=dict_strat)
         self.assertEqual(gold, output)
 
     def test_nested_master_has_fewer(self):
@@ -112,7 +136,11 @@ class test_merge(unittest.TestCase):
                 }
             }
         }
-        output = merge(dict1, dict2)
+
+        def dict_strat(dict1, dict2) -> dict:
+            return merge(dict1, dict2)
+
+        output = merge(dict1, dict2, dict_strategy=dict_strat)
         self.assertEqual(gold, output)
 
     def test_merge_array_strategy_default(self):
@@ -137,13 +165,140 @@ class test_merge(unittest.TestCase):
             "foo": [3, 4]
         }
 
-        array_strategy = lambda x, y: reduce(lambda i, j: i+j, [x, y])
+        list_strategy = lambda x, y: reduce(lambda i, j: i+j, [x, y])
 
         gold = {
             "foo": [1, 2, 3, 4]
         }
 
-        output = merge(dict1, dict2, array_strategy=array_strategy)
+        output = merge(dict1, dict2, list_strategy=list_strategy)
+        self.assertEqual(gold, output)
+
+    def test_merge_assortment(self):
+        dict1 = {
+            "foo": {
+                "bar": [1, 2]
+            },
+            "qux": "sup"
+        }
+        dict2 = {
+            "foo": {
+                "bar": [3, 4],
+                "baz": "hello"
+            },
+            "pi": 3.14
+        }
+
+        list_strategy = lambda x, y: reduce(lambda i, j: i+j, [x, y])
+        def dict_strategy(x, y):
+            return merge(x, y, dict_strategy=dict_strategy, list_strategy=list_strategy)
+
+        gold = {
+            "foo": {
+                "bar": [1, 2, 3, 4],
+                "baz": "hello"
+            },
+            "qux": "sup",
+            "pi": 3.14
+        }
+
+        output = merge(dict1, dict2, dict_strategy=dict_strategy, list_strategy=list_strategy)
+        self.assertEqual(gold, output)
+
+
+class test_merge_map(unittest.TestCase):
+
+    def test_null_case_left(self):
+        left = 1
+        right = None
+        gold = 1
+        output = merge_map(left, right)
+        self.assertEqual(gold, output)
+
+    def test_null_case_right(self):
+        left = None
+        right = 1
+        gold = 1
+        output = merge_map(left, right)
+        self.assertEqual(gold, output)
+
+    def test_primitive(self):
+        left = 1
+        right = 2
+        gold = 1
+        output = merge_map(left, right)
+        self.assertEqual(gold, output)
+
+    def test_dict_default(self):
+        left = {"foo": "bar"}
+        right = {"baz": "qux"}
+        gold = {"foo": "bar"}
+        output = merge_map(left, right)
+        self.assertEqual(gold, output)
+
+    def test_array_default(self):
+        left = [1, 2]
+        right = [3, 4]
+        gold = [1, 2]
+        output = merge_map(left, right)
+        self.assertEqual(gold, output)
+
+
+class test_get_keys(unittest.TestCase):
+
+    def test_null_case(self):
+        left = {}
+        right = {}
+        gold = set()
+        output = get_keys(left, right)
+        self.assertEqual(gold, output)
+
+    def test_left_null(self):
+        left = {}
+        right = {"foo": "bar"}
+        gold = {"foo"}
+        output = get_keys(left, right)
+        self.assertEqual(gold, output)
+
+    def test_right_null(self):
+        left = {"foo": "bar"}
+        right = {}
+        gold = {"foo"}
+        output = get_keys(left, right)
+        self.assertEqual(gold, output)
+
+    def test_union(self):
+        left = {"foo": "bar"}
+        right = {"baz": "qux"}
+        gold = {"foo", "baz"}
+        output = get_keys(left, right)
+        self.assertEqual(gold, output)
+
+
+class test_get_values(unittest.TestCase):
+
+    def test_null_left(self):
+        left = {}
+        right = {"foo": 1}
+        key = "foo"
+        gold = (None, 1)
+        output = get_values(left, right, key)
+        self.assertEqual(gold, output)
+
+    def test_null_right(self):
+        left = {"foo": 1}
+        right = {}
+        key = "foo"
+        gold = (1, None)
+        output = get_values(left, right, key)
+        self.assertEqual(gold, output)
+
+    def test_both_exist(self):
+        left = {"foo": 1}
+        right = {"foo": 2}
+        key = "foo"
+        gold = (1, 2)
+        output = get_values(left, right, key)
         self.assertEqual(gold, output)
 
 
